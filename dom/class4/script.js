2b5ccf29b9724a01930dc3a5e7da74b74d806514
Haseeb <haseebnazir437@gmail.com>
2026-02-19 22:20:36 +0500

    class 4

---
+++ dom/class4/script.js
@@ -0,0 +1,49 @@
+// ========================== HTML Attributes
+
+// In HTML, tags may have attributes. When the browser parses the HTML to create DOM objects for tags, it recognizes standard attributes and creates DOM properties from them.
+
+// So when an element has id or another standard attribute, the corresponding property gets created. But that doesn’t happen if the attribute is non-standard.
+
+// let div = document.querySelector("#container");
+// console.log(div.id);        // standard attribute give id name
+
+// let div1 = document.querySelector("#container");
+// console.log(div1.check);    // non standard attribute return undefined
+
+// ========================= Methods
+
+// ========================= elem.hasAttribute(name) 
+
+//  checks for existence.
+
+// let div1 = document.querySelector("#container");
+
+// console.log(div1.hasAttribute("check"));     // true
+// console.log(div1.hasAttribute("something"));   // false
+ 
+// ======================== elem.getAttribute(name) 
+
+//  gets the value.
+
+// let div1 = document.querySelector("#container");
+
+//  console.log(div1.getAttribute("check"));  // value    non-standard
+
+// ======================== elem.setAttribute(name, value) 
+
+//  sets the value.
+
+// let div1 = document.querySelector("#container");
+
+//  console.log(div1.setAttribute("check", "public"));     // value of check changed to public
+
+// console.log(document.body);
+
+// ======================== elem.removeAttribute(name)
+
+//  removes the attribute.
+
+// let div1 = document.querySelector("#container");
+
+//  div1.removeAttribute("check");     // check attribute removed
+// console.log(document.body);
