cc59e7deaea6285974bd72f38975e836d4907c37
Haseeb <haseebnazir437@gmail.com>
2026-02-21 10:45:25 +0500

    class 1

--- dom/class1/script.js
+++ dom/class1/script.js
@@ -1,12 +1,12 @@
 // ==================================== Walking the DOM
 
-// console.log(document.documentElement);    // returns HTML node
+// console.log(document.documentElement);    // access HTML node
 
-// console.log(document.body);    // returns body node
+// console.log(document.body);    // access body node
 
-// console.log(document.head);    // returns head node
+// console.log(document.head);    // access head node
 
-// console.log(document.title);   // returns string title
+// console.log(document.title);   // access string title
 
 // if we write or add Js before body tag then body node will be null because we accessed document.body before body tag
 
@@ -25,7 +25,7 @@
 // space before and after tag counted as child node 
 // comments are also counted as child
 
-// console.log(document.body.childNodes);    // accessing all children returns nodelist not array
+// console.log(document.body.childNodes);        // accessing all children returns nodelist not array to iterate nodelist loops and array methods are used after converting it to array 
 
 // console.log(document.body.childNodes[1]);    // accessing child by index no.
 
@@ -34,6 +34,37 @@
 // console.log(document.body.lastChild);    // accessing last child
 
 // console.log(document.body.hasChildNodes());  // tells element has childnode or empty in form of true and false
+
+// =================================== Parent and siblings 
+
+
+// console.log(document.body.parentNode);          // to access parent node  
+
+// console.log(document.body.previousSibling);     // to access previous sibling node
+
+// console.log(document.body.nextSibling);     // to access next sibling node
+
+// ================================== Element-only navigation 
+
+// to access only element of html (not text or comment node)
+
+// console.log(document.body.parentElement);          // to access parent element
+
+// console.log(document.body.children);   //  returns collection of elements
+
+// console.log(document.body.firstElementChild);   // access first element child
+
+// console.log(document.body.lastElementChild);   // access last element child
+
+// console.log(document.body.previousElementSibling);     // to access previous sibling Element node
+
+// console.log(document.body.nextElementSibling);     // to access next sibling Element node
+
+
+
+
+
+
 
 
 
