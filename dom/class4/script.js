01707f3fe57f019a8fb694640b180c015043e7d6
Haseeb <haseebnazir437@gmail.com>
2026-02-24 07:18:15 +0500

    class 4

--- dom/class4/script.js
+++ dom/class4/script.js
@@ -194,3 +194,65 @@
 
 // ===================================== DocumentFragment()
 
+// When you add elements directly to the DOM one by one, the browser may re-render multiple times.
+// With a DocumentFragment, you:
+
+// Create elements
+
+// Append them to the fragment
+
+// Append the fragment to the DOM once
+
+// This reduces reflows and speeds things up
+
+// let ul = document.createElement('ul');
+// let frag = document.createDocumentFragment();
+
+// let item1 = document.createElement('li');
+// let item2 = document.createElement('li');
+
+// item1.textContent = "Apples";
+// item2.textContent = "Bananas";
+
+// ul.append(item1);
+// ul.append(item2);
+
+// frag.append(ul);
+// document.body.append(frag);
+
+// ============================================= Styles and classes
+
+// JavaScript can modify both classes and style properties
+
+// =========================== className and classList
+
+// If we assign something to elem.className, it replaces the whole string of classes
+
+// let hey = document.body.querySelector(".heyme");
+
+// hey.className = "the";
+
+// The elem.classList is a special object with methods to add/remove/toggle a single class
+
+// hey.classList.add('good');
+// hey.classList.remove('heyme')
+// hey.classList.toggle("bye") ;         // adds the class if it doesn’t exist, otherwise removes it.
+// console.log(hey.classList.contains('heyme')); // checks for the given class, returns true/false
+
+// ========================== Element style
+
+// The property elem.style is an object that corresponds to what’s written in the "style" attribute
+
+// let div = document.body.querySelector("#container");
+
+// div.style.color = "white";
+// div.style.background = "green";
+// div.style.width = "50%";
+
+// // ========= getComputedStyle
+
+// let computedStyle = getComputedStyle(div);
+
+// alert(computedStyle.color);
+// alert(computedStyle.background);
+// alert(computedStyle.width);
