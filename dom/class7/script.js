2401c94cceef4ce8f0aa4ca7463507b9f5214470
Haseeb <haseebnazir437@gmail.com>
2026-02-27 07:08:29 +0500

    form

--- dom/class7/script.js
+++ dom/class7/script.js
@@ -6,7 +6,7 @@
 
 // console.log(document.forms[0]);    // return form on given index
 
-let forms = document.forms[0]
+// let forms = document.forms[0]
 
 // console.log(forms.elements);          // return collection of form elements
 
@@ -16,7 +16,7 @@ let forms = document.forms[0]
 
 // console.log(nme);
 
-let nme= forms.elements.gender;
+// let nme= forms.elements.gender;
 
 // console.log(nme[1].value);               // returns value of accesed element
 
@@ -25,5 +25,34 @@ let nme= forms.elements.gender;
 
 // form[index/name]                // access element
 
-// console.log(forms[1]);
+// console.log(forms[1]);              //   returns element of given index
 // console.log(forms['gender']);       //   returns collection of elements of given name
+
+// back reference
+// let user =userinfo.uname;
+// console.log(user.form);
+
+//   select.options[0].selected = true;      // all three used for same  
+//   select.selectedIndex = 1;
+//   select.value = "banana";
+
+// add new option to select
+
+// let newOption = new Option("Classic", "classic",false,false);
+//   genres.append(newOption);
+
+
+
+let upassword = document.querySelector("#upswd");
+let btn = document.querySelector("#btn");
+
+btn.addEventListener("click", function() {
+
+    if (upassword.type === "password") {
+        upassword.type = "text";
+        btn.textContent = "Hide";
+    } else {
+        upassword.type = "password";
+        btn.textContent = "Show";
+    }
+});
