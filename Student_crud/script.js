be43465878961a0d25f54da406c0d52036f4d248
Haseeb <haseebnazir437@gmail.com>
2026-03-07 21:23:50 +0500

    clone

--- Student_crud/script.js
+++ Student_crud/script.js
@@ -1,4 +1,5 @@
 let students = JSON.parse(localStorage.getItem("students")) || [];
+
 rendertable();
 
 $('#infoForm').on("submit", function (event) {
@@ -8,20 +9,24 @@ $('#infoForm').on("submit", function (event) {
     let sname = $('#name').val().trim();
     let age = $('input[name=studentage]').val().trim();
     let marks = $('input[name=studentMarks]').val().trim();
+    let percentage = (marks / 1100) * 100;
 
     if (sname === "" || age === "" || marks === "") {
         alert("Enter Valid Info");
         return;
     }
 
-    let percentage = (marks / 1100) * 100;
 
     const student = {
+
         stuName: sname,
         stuAge: age,
         stuMarks: marks,
         stuPercentage: percentage.toFixed(2) + "%"
+
     };
+
+
 
     let editindex = $('#editIndex').val().trim();
 
@@ -41,7 +46,8 @@ $('#infoForm').on("submit", function (event) {
 
     rendertable();
 
-    this.reset()
+    this.reset();
+
 });
 
 function rendertable() {
@@ -86,26 +92,39 @@ function rendertable() {
 }
 
 
-$(document).on('click','.delete',function(e){
-let index = $(this).data('index');
-students.splice(index,1);
-localStorage.setItem("students", JSON.stringify(students));
+$(document).on('click', '.delete', function (e) {
 
-rendertable();
+    let index = $(this).data('index');
+    students.splice(index, 1);
+    localStorage.setItem("students", JSON.stringify(students));
+
+    rendertable();
 });
 
-$(document).on('click','.edit',function(){
-    let index =$(this).data('index');
-    let studentedit =students[index];
+
+$(document).on('click', '.edit', function () {
+
+    let index = $(this).data('index');
+    let studentedit = students[index];
 
     $('#name').val(studentedit.stuName);
     $("input[name='studentage']").val(studentedit.stuAge);
     $("input[name='studentMarks']").val(studentedit.stuMarks);
 
     $('#editIndex').val(index);
-   $("#saveBtn").text("Update");
+    $("#saveBtn").text("Update");
 
 })
 
 
+$(document).on('click', '.clone', function () {
+
+    let index = $(this).data('index');
+
+    let student = { ...students[index] };
+
+    students.push(student);
+    rendertable();
+    
+})
 
